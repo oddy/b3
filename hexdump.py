4505c662c77c2e41293b41b8afc841383fc30cd8
Beau Butler <beau.butler@gmail.com>
2020-03-10 00:05:54 +1300

    Tuning hexdump

--- hexdump.py
+++ hexdump.py
@@ -9,21 +9,23 @@ cols = { 'rst':'\x1b[0m', 'gry':'\x1b[2;37m', 'red':'\x1b[31m', 'yel':'\x1b[0;33
          'grn':'\x1b[0;32m', 'bgrn':'\x1b[1;32m', 'blu':'\x1b[0;34m',  'bblu':'\x1b[34;1m' }
 
 colOK = hasattr(sys.stdout,'isatty') and sys.stdout.isatty()
+INT_PRINTABLE = [ord(i) for i in string.printable]
 
 # Note: bytes accessess -> int in py3, -> str in py2.
 if PY2:
     def dot_str_from_bytes(s):
         return ''.join([i if (ord(i)>13 and i in string.printable) else '.' for i in s])
 else:
-    INT_PRINTABLE = [ord(i) for i in string.printable]
-
     def dot_str_from_bytes(s):
         return ''.join([chr(i) if (i>13 and i in INT_PRINTABLE) else '.' for i in s])
 
 
 def hexdump(prefix, src, length=16):
-    if src and not isinstance(src, bytes):
+    if not src:
+        raise ValueError('Empty input to hexdump')
+    if not isinstance(src, bytes):
         raise TypeError('Input to hexdump must be bytes, not %s' % type(src))
+
     N=0; result=''
     while src:
         s,src = src[:length],src[length:]
@@ -45,13 +47,12 @@ def hexdump(prefix, src, length=16):
             hexa += cols['rst']
         hexa += ' ' * (length*3 - nccl)     # pad\
         s2 = dot_str_from_bytes(s)
-        result += "%s %04X %s   %s\n" % (prefix, N, hexa, s2)
+        line = "%s %04X %s   %s\n" % (prefix, N, hexa, s2)
+        result += line
         N += length
 
     return result.strip()
 
 
 if __name__ == '__main__':
-    print(hexdump('a', b'hello world'))
-
-    #print(hexdump('',sys.stdin.read()))
+    print(hexdump('+', b'hello world\r\nFoo Bar\xff\xff\xff\xff testing 1 2 3 is this thing on testing'))
