daa13e571719d4f0c2df06caae90e598afb736d6
Beau Butler <beau.butler@gmail.com>
2020-05-04 04:11:53 +1200

    general part of SCHED encoding

--- datatypes.py
+++ datatypes.py
@@ -1,4 +1,4 @@
-import struct, decimal
+import struct, decimal, zlib
 #from six import PY2\
 import six
 from   six import int2byte
@@ -79,26 +79,26 @@ def GuessType(obj):
     raise NotImplementedError('Unknown type %r' % type(obj))
 
 
-
-# All these Out bytes.
+# ---------------------------------- Decimal --------------------------------------------------------
 
 # i say we DONT take floats if it's too hard to convert them / get the info we need out of them
 # / have to get precision out of them.
 
-# In: num - a float/double or decimal.Decimal type
+# Top 4bits:
+# bit 4 (0x80) : 0=number, 1=special
+# bit 3 (0x40) : 0=+ve number, 1=-ve number
+# bit 2 (0x20) : [number] 0=+ve expo 1=-ve expo   [special] 0=nan 1=infinity
+# bit 1 (0x10) : [number] 0=expo bottom-4bits 1=expo varint follows  [special] 0=qnan 1=snan
+
+# Bottom 4bits:
+# [number]  exponent if top bit1=0, unused otherwise
+# [special] unused.
+
+# In:  num - a decimal.Decimal type ONLY
+# Out: bytes
 def encode_decimal(num):
     if not isinstance(num, decimal.Decimal):
         raise TypeError("only accepts decimal.Decimal objects")
-
-    # Top 4bits:
-    # bit 4 (0x80) : 0=number, 1=special
-    # bit 3 (0x40) : 0=+ve number, 1=-ve number
-    # bit 2 (0x20) : [number] 0=+ve expo 1=-ve expo   [special] 0=nan 1=infinity
-    # bit 1 (0x10) : [number] 0=expo bottom-4bits 1=expo varint follows  [special] 0=qnan 1=snan
-
-    # Bottom 4bits:
-    # [number]  exponent if top bit1=0, unused otherwise
-    # [special] unused.
 
     sign,digits,exp = num.as_tuple()
     special   = not num.is_finite()
@@ -141,9 +141,11 @@ def encode_decimal(num):
 
     return b''.join(out)
 
+# In:  bytes
+# Out: a decimal.Decimal
+def decode_decimal(buf):
+    return None
 
-
-# do math using decimal context or 'normal' float math, depending on what comes in.
 
 # In:  time.time-style float.
 # Note: Current policy is we're not taking datetimes for here. They get turned into SCHEDs by the auto, and if
@@ -154,9 +156,108 @@ def encode_stamp64(timestamp):
     raise TypeError('STAMP64 only accepts floats, please use SCHED or convert yourself first')
 
 
-# In: datetime.  maybe a time.time-style float, but probably not.
-def encode_sched(datetime):         # this one might need its own file
-    return b'\x69'
+# In: a datetime or a date object.
+
+# In: time in units since 1970, whether the units are days or [sub]seconds, the exponent of subsecondness (0=seconds)
+#     the tz offset in -05:45 string format, the tzname in string format.
+
+# sign for time in units since 1970
+
+
+MINUTE_BITS = {'00':0x00, '15':0x10, '30':0x20, '45':0x30}
+
+def encode_sched_gen(num, is_days=False, subsec_exp=0, tzoff='', tzname=''):
+    bits = 0x00
+    if is_days:                         # bit 4 days-or-seconds
+        bits |= 0x80
+    if tzoff:                           # bit 4 timezone offset present
+        bits |= 0x40
+    if tzname:                          # bit 2 tzname present
+        bits |= 0x20
+    if num < 0:                         # bit 1
+        bits |= 0x10                    # consider svarint-ing num if we need a bit back.
+
+    subsec_exp = abs(subsec_exp)        # it's ALWAYS to the -ve so we dont actually need a sign for it.
+    bits |= (subsec_exp & 0x0f)         # likewise we can cram this to sec/milli/micro/nano if we need more bits.
+
+    print 'bits = ',bits
+    out = [int2byte(bits), encode_uvarint(num)]
+
+    # d.tzinfo.utcoffset(d) gives a timedelta in seconds. e.g. nepal Asia/Kathmandu is 20700 sec
+    # d.strftime("%z")  gives utcoffset string "+hhmm", assume we're using that for now.
+    if tzoff:
+        offbyte = 0x00
+        if tzoff[0] == "-":
+            offbyte |= 0x80                         # sign bit
+        # ??? dst bit ??? would go here.
+        min_str = tzoff[3:5]
+        offbyte |= (0x30 & MINUTE_BITS[min_str])    # minutes bits
+        hour_str = tzoff[1:3]
+        offbyte |= (0x0f & int(hour_str))           # hour bits
+        out.append(int2byte(offbyte))
+
+    if tzname:
+        ncrc = zlib.crc32(tzname) & 0xffffffff
+
+        out.append(struct.pack('<L', ncrc))
+
+    return b''.join(out)
+
+
+    # d_naive = datetime.datetime.now()
+    # timezone = pytz.timezone("America/Los_Angeles")
+    # d_aware = timezone.localize(d_naive)
+
+    # dnepal.strftime("%z")  -> "+0545"   from the object itself.
+
+    # >>> pytz.timezone('US/Samoa').localize(datetime.datetime.now()).strftime("%z")
+
+
+    # TEST: datatypes.encode_sched_gen(44, False, -9, '+1145', 'Asia/Kathmandu' )
+
+
+# when do varints jump to 3 bytes.  uvarint: 16384  svarint: 8192
+# number of days is 19385
+
+
+
+# Control byte top 4bits:
+# bit 4   days-or-seconds
+# bit 3   offset present
+# bit 3   tzname present
+# bit 1...tzname format 0=crc32, 1=something else
+
+# Control byte bottom 4bits:
+#         exponent number for arb prec main value - bc windows for example has 100-ns values
+
+# =========
+# offset byte:
+# sign bit,  4bits of 12 hours, 2 bits of minutes, 1 bit for dst maybe. (still dont know about this)
+# ===========
+# tzname:
+# 4-bytes of crc32  if control bit1=0,  othwerwise size varint followed by a string.
+
+
+
+# YES to control byte
+# 2? bits for main scale - day, sec, nano
+
+# naive vs aware flag?  - no the presence/absence of timezone data itself indicates this
+
+#
+
+
+
+
+# utc time
+# offset
+# tzname
+
+
+# in py3: zlib.crc32(b'America/North_Dakota/New_Salem')   but you can still do the & 0xfffff
+# in py2: zlib.crc32(b'America/North_Dakota/New_Salem') & 0xffffffff  to get unsigned.
+# in go: crc32.ChecksumIEEE([]byte("America/North_Dakota/New_Salem")))
+
 
 # In: a python complex number object. Must have real and imag properties
 def encode_complex(cplx):
