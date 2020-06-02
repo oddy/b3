31c103327e8d374707f3d2ff12132359cb26b121
Beau Butler <beau.butler@gmail.com>
2020-06-02 14:25:35 +1200

    Item header encode and decode tests, no longer using types.NoneType because py3 doesnt have it and 'is None' is more idiomatic anyway

--- item_header.py
+++ item_header.py
@@ -1,21 +1,30 @@
 
 from   six import PY2, int2byte, byte2int
-from types import NoneType
 
 from datatypes import IntByteAt, VALID_STR_TYPES, VALID_INT_TYPES
 from type_varint import encode_uvarint, encode_svarint, decode_svarint, decode_uvarint
 
-# --- Item header byte ---
+# --- item structure ---
+# [header BYTE] [key (see below)] [data len UVARINT] [data BYTES]
+# \------------------------------------------------/                            = handled here
+
+# --- header byte ---
 # +------------+------------+------------+------------+------------+------------+------------+------------+
 # | key type   | key type   | ?        ? |  data type | data type  | data type  | data type  | data type  |
 # +------------+------------+------------+------------+------------+------------+------------+------------+
+
+# --- Key bits   &   structure ---
+#     0   0  (0)     no bytes
+#     0   1  (4)     UVARINT
+#     1   0  (8)     UTF8 bytes
+#     1   1  (c)     raw bytess
 
 # Note: for now we are keeping it as simple as possible.
 # Note: we can experiment with super-compact options like 'zero flag' and 'size present flag' and 'true type false type' once we're DONE
 # Note: and we will use the C3 standard as our test data for compactifying.
 
 
-def encode_item_header(key, data_type, data_len):                       # user_bit=0
+def encode_header(key, data_type, data_len):                       # user_bit=0
     key_type_bits, key_bytes = encode_key(key)
     cbyte = 0x00
     cbyte |= key_type_bits & 0xc0                      # top 2 bits key type
@@ -24,22 +33,22 @@ def encode_item_header(key, data_type, data_len):                       # user_b
     return int2byte(cbyte) + key_bytes + len_bytes
 
 
-def decode_item_header(buf, index):
+def decode_header(buf, index):
     cbyte,index = IntByteAt(buf, index)                # control byte
     key,index = decode_key(cbyte & 0xc0, buf, index)   # key bytes
     data_len,index = decode_uvarint(buf, index)        # data len bytes
     data_type = cbyte & 0x1f
-    return key, data_type, data_len                    # but not the data bytes themselves, because the decoders nom them.
+    return key, data_type, data_len, index                    # but not the data bytes themselves, because the decoders nom them.
 
 
 # Out: the key type bits, and the key bytes.
 
 def encode_key(key):
     ktype = type(key)
-    if ktype == NoneType:
+    if key is None:               # this is more idiomatic python than 'NoneType' (which is gone from types module in py3)
         return 0x00, b""
     if ktype in VALID_INT_TYPES:
-        return 0x40, encode_svarint(key)
+        return 0x40, encode_uvarint(key)
     if ktype in VALID_STR_TYPES:
         keybytes = key.encode("utf8","replace")
         return 0x80, encode_uvarint(len(keybytes)) + keybytes
@@ -48,11 +57,13 @@ def encode_key(key):
     raise TypeError("Key type must be None, int, str or bytes, not %s" % ktype)
 
 
+# Out: the key, and the new index
+
 def decode_key(key_type_bits, buf, index):
     if key_type_bits == 0x00:
         return None, index
     if key_type_bits == 0x40:
-        return decode_svarint(buf, index)            # note returns number, index
+        return decode_uvarint(buf, index)            # note returns number, index
     if key_type_bits == 0x80:
         klen,index = decode_uvarint(buf, index)
         key_str_bytes = buf[index:index+klen]
@@ -64,6 +75,7 @@ def decode_key(key_type_bits, buf, index):
     raise TypeError("Invalid key type in control byte")
 
 
+# Note: discussion about size bits and zero bits and funky stuff like that
 # Size bit keeps us future proof! WHILE enabling the option of some types having no size!
 # Currently the only types that have no size are None and B3_END
 # (Actually if we have a size bit, we can have a whole different registry of unsized types!  This is more complex tho.)
--- test_header.py
+++ test_header.py
@@ -7,13 +7,13 @@ from item_header import *
 
 def test_header_key_enc_none_int():
     assert encode_key(None)         == (0x00, b"")
-    assert encode_key(4)            == (0x40, SBytes("08"))               # py2 int
-    assert encode_key(7777777777)   == (0x40, SBytes("e2 e1 bb f9 39"))   # py2 long
+    assert encode_key(4)            == (0x40, SBytes("04"))               # py2 int
+    assert encode_key(7777777777)   == (0x40, SBytes("f1 f0 dd fc 1c"))   # py2 long
 
 def test_header_key_dec_none_int():
     assert decode_key(0x00, b"", 0)                         == (None, 0)
-    assert decode_key(0x40, SBytes("08"), 0)                == (4, 1)
-    assert decode_key(0x40, SBytes("e2 e1 bb f9 39"), 0)    == (7777777777, 5)
+    assert decode_key(0x40, SBytes("04"), 0)                == (4, 1)
+    assert decode_key(0x40, SBytes("f1 f0 dd fc 1c"), 0)    == (7777777777, 5)
 
 
 def test_header_key_enc_str_bytes():
@@ -27,5 +27,13 @@ def test_header_key_dec_str_bytes():
     assert decode_key(0xc0, SBytes("03 66 6f 6f"),0)                                == (b"foo", 4)
 
 
+def test_header_enc():
+    assert encode_header(key=None,   data_type=5,  data_len=5)    == SBytes("05 05")
+    assert encode_header(key=5,      data_type=5,  data_len=5)    == SBytes("45 05 05")
+    assert encode_header(key=u"foo", data_type=28, data_len=1500) == SBytes("9c 03 66 6f 6f dc 0b")
 
+def test_header_dec():
+    assert decode_header(SBytes("05 05"),0)                 == (None, 5, 5, 2)
+    assert decode_header(SBytes("45 05 05"),0)              == (5, 5, 5, 3)
+    assert decode_header(SBytes("9c 03 66 6f 6f dc 0b"),0)  == (u"foo", 28, 1500, 7)
 
