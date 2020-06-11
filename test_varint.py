0935ccc7c30d77e14ca65a0b950bb12a7bba7e92
Beau Butler <beau.butler@gmail.com>
2020-06-11 12:52:51 +1200

    tests for varint internal & codec are passing again post-end API change

--- test_varint.py
+++ test_varint.py
@@ -21,6 +21,11 @@ def test_svarint_enc():
     assert encode_svarint(123456789)  == SBytes("aa b4 de 75")
     assert encode_svarint(-123456789) == SBytes("a9 b4 de 75")
 
+def test_varint_codec_dec():                           # just making sure the index isnt returned
+    assert codec_decode_svarint(SBytes("aa b4 de 75"), 0, 4)  == 123456789
+    assert codec_decode_svarint(SBytes("a9 b4 de 75"), 0, 4)  == -123456789
+    assert codec_decode_uvarint(SBytes("d0 86 03"), 0, 4) == 50000
+
 def test_svarint_dec():
     assert decode_svarint(SBytes("64"), 0)       == (50, 1)
     assert decode_svarint(SBytes("63"), 0)       == (-50, 1)
