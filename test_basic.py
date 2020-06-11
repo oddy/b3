6359e9fea39c5b4b1d08c1ae5d65c7197a364e63
Beau Butler <beau.butler@gmail.com>
2020-06-11 13:14:21 +1200

    basic type tests fixed

--- test_basic.py
+++ test_basic.py
@@ -4,27 +4,14 @@
 from utils import SBytes
 from type_basic import *
 
-def test_base_null_enc():
-    assert encode_null(None) == b""
-
-def test_base_null_dec():
-    assert decode_null(b"", 0, 0) == (None, 0)
-
 
 def test_base_bool_enc():
     assert encode_bool(True)    == SBytes("01")
     assert encode_bool(False)   == SBytes("00")
 
 def test_base_bool_dec():
-    assert decode_bool(SBytes("01"),0,1) == (True, 1)
-    assert decode_bool(SBytes("00"),0,1) == (False,1)
-
-
-TESTB = b"hello"
-def test_base_bytes_enc():
-    assert encode_bytes(TESTB) == TESTB
-def test_base_bytes_dec():
-    assert decode_bytes(TESTB,0,len(TESTB)) == (TESTB, len(TESTB))
+    assert decode_bool(SBytes("01"),0,1) == True
+    assert decode_bool(SBytes("00"),0,1) == False
 
 
 #
@@ -42,7 +29,7 @@ def test_base_utf8_enc():
 
 def test_base_utf8_dec():
     for tstr,tbytes in TEST_UNISTRS:
-        assert decode_utf8(tbytes,0,len(tbytes)) == (tstr,len(tbytes))
+        assert decode_utf8(tbytes,0,len(tbytes)) == tstr
 
 
 TEST_INT64S = (
@@ -58,7 +45,7 @@ def test_base_int64_enc():
 
 def test_base_int64_dec():
     for tint,tbytes in TEST_INT64S:
-        assert decode_int64(tbytes,0,8) == (tint, 8)
+        assert decode_int64(tbytes,0,8) == tint
 
 
 TEST_FLOAT64S = (
@@ -72,7 +59,7 @@ def test_base_float64_enc():
 
 def test_base_float64_dec():
     for tflo,tbytes in TEST_FLOAT64S:
-        assert decode_float64(tbytes,0,8) == (tflo, 8)
+        assert decode_float64(tbytes,0,8) == tflo
 
 
 # stamp64 is signed, so it's 2**63 ns / 1000 / 1000 / 1000 / 60 / 60 / 24 / 365 = about 292.5 years each side of 1970
@@ -87,7 +74,7 @@ def test_base_stamp64_enc():
     assert encode_stamp64(timNano) == SBytes("00 22 46 6c ad d1 13 16")
 
 def test_base_stamp64_dec():
-    assert decode_stamp64(SBytes("00 22 46 6c ad d1 13 16"),0,8)  == (timNano,8)
+    assert decode_stamp64(SBytes("00 22 46 6c ad d1 13 16"),0,8)  == timNano
 
 
 tcplx = 13.37+42.42j
@@ -97,6 +84,6 @@ def test_base_complex_enc():
     assert encode_complex(tcplx) == tcplx_bytes
 
 def test_base_complex_dec():
-    assert decode_complex(tcplx_bytes, 0, 16) == (tcplx, 16)
+    assert decode_complex(tcplx_bytes, 0, 16) == tcplx
 
 
