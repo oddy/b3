d3cf0c074e1723b49f1734fea031925e5b1f5b01
Beau Butler <beau.butler@gmail.com>
2020-06-18 12:58:07 +1200

    Surfaced the actual package UX in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,19 @@
 
 # B3 Public API
 
-# todo: the module's actual UX
+# todo: the packages's actual UX
 
+from datatypes import *
 from composite_dynamic import pack, unpack
 from composite_schema import schema_pack, schema_unpack
+
+__all__ = ['pack', 'unpack',
+           'schema_pack', 'schema_unpack',
+           "B3_COMPOSITE_DICT","B3_COMPOSITE_LIST",
+           "B3_BYTES", "B3_UTF8","B3_BOOL",
+           "B3_INT64","B3_UVARINT","B3_SVARINT",
+           "B3_FLOAT64","B3_DECIMAL",
+           "B3_STAMP64","B3_VARSTAMP","B3_SCHED",
+           "B3_COMPLEX"
+           ]
 
