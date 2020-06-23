5fa4a9704c584d687d202a7b8aca0a9dc623008e
Beau Butler <beau.butler@gmail.com>
2020-06-24 00:56:12 +1200

    Made the examples a little clearer

--- examples.py
+++ examples.py
@@ -1,5 +1,5 @@
-# B3 Usage examples
-# =================
+
+# ================== B3 Usage examples ==================
 
 from __future__ import print_function
 import decimal, datetime
@@ -7,21 +7,20 @@ from pprint import pprint
 
 import b3
 
+# ==================== EASY MODE ========================
 # You can pack lists of things (like json.dumps):
 
 list_data = [ None,  b"foo",  u"bar",  True,  -69,  2.318,  46j,  [1,2,3],  {4:5, 6:7},
               decimal.Decimal("13.37"), datetime.datetime.now() ]
-            # Complex numbers, decimal numbers, and dates and times all work.
+# Complex numbers, decimal numbers, and dates and times all work.
 
 list_buf = b3.pack(list_data)
-
 
 # You can pack dicts of things:
 
 dict_data = { 1:1, u"2":u"2", b"3":b"3" }       # byte keys are supported as well as string and number keys
 
 dict_buf = b3.pack(dict_data)
-
 
 # You can save on slicing when unpacking by giving unpack a start index:
 
@@ -38,6 +37,8 @@ print("\nDict matches?")
 print(repr(dict_data == out_dict))
 
 
+
+# ==================== SCHEMA MODE ======================
 # You can make messages using a "type, name, tag_number" schema (like protobuf):
 # (See datatypes.py for the available types)
 
@@ -59,6 +60,9 @@ out_sch = b3.schema_unpack(SCHEMA, sch_buf, 0, len(sch_buf))
 print("\nSchema matches?")
 print(repr(sch_data == out_sch))
 
+
+
+# ==================== SCHEMA NESTING ===================
 # Schema is intended for flat data structures, but nesting is still straightforward,
 # You can pack nested fields first, building bottom-up:
 
@@ -73,7 +77,6 @@ inner_buf  = b3.schema_pack(SCHEMA, inner_data)
 outer_data = dict(index=23, label=u"ss", inner_message=inner_buf)
 outer_buf  = b3.schema_pack(OUTER_SCHEMA, outer_data)
 
-
 # Unpacking is the reverse, so a little bit manual, but still easy:
 
 ret_outer  = b3.schema_unpack(OUTER_SCHEMA, outer_buf, 0, len(outer_buf))
@@ -85,6 +88,8 @@ print("inner ",repr(inner_data == ret_inner))
 print("outer ",repr(outer_data == ret_outer))
 
 
+
+# ==================== INTEROP (WIP) ====================
 # schema_unpack() can unpack messages from pack() if you keep a few simple rules in mind. (use dicts!)
 # This means you can start with json-like and 'upgrade' to schemas later, or have "quick-n-dirty" clients talking to schema-ed servers.
 # Going the other way (schema_pack() -> unpack()) is harder but doable if you're prepared to accept tag numbers as keys instead of string names.
